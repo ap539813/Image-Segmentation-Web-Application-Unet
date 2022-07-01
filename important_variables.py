8a0f496dc9ca96715e08f6770581a531b06febcd
Ankush Pandey <ap539813@gmail.com>
2022-07-01 19:02:23 +0530

    important variable and utilities are created

---
+++ important_variables.py
@@ -0,0 +1,29 @@
+from keras import backend as K
+import base64
+
+# Define the hight and width of the images as required
+
+img_width, img_height = 256,256
+
+input_shape = (img_width, img_height)
+
+# dimensions of our images.
+# if K.image_data_format() == 'channels_first':
+#     input_shape = (3, img_width, img_height)
+# else:
+#     input_shape = (img_width, img_height, 3)
+
+
+theme_image_name = 'Plant disease classification.png'
+
+"""### gif from local file"""
+file_ = open(theme_image_name, "rb")
+contents = file_.read()
+data_url = base64.b64encode(contents).decode("utf-8")
+file_.close()
+
+
+css_file_path = 'style/style.css'
+
+model_path = 'Unet_model.hdf5'
+
