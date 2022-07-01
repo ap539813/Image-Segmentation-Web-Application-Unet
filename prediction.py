555441335f3ab099805ebd387f79643c46105716
Ankush Pandey <ap539813@gmail.com>
2022-07-01 19:03:13 +0530

    prediction module takes care of the predition in the app

---
+++ prediction.py
@@ -0,0 +1,32 @@
+from create_Segmentation_model import my_Unet
+from important_variables import img_height, img_width
+import streamlit as st
+from PIL import Image
+import numpy as np
+
+
+
+def segment_image(model_path):
+    model = my_Unet(model_path)
+
+    input_col, output_col = st.columns([1, 1])
+
+    input_col.markdown('## Input Image')
+    output_col.markdown('## Segmented Image')
+    uploaded_file = input_col.file_uploader("Choose an image...", type="jpg")
+    if uploaded_file is not None:
+        # image = cv2.imread(uploaded_file)
+        image = Image.open(uploaded_file)
+        input_col.image(image, caption='Input Image.', use_column_width=True)
+        st.write("")
+        image = image.resize((img_width, img_height))
+        
+        image = np.expand_dims(image, 0)/255
+
+        
+        pred = model.predict(image)[0,:,:]
+
+        output_col.image(pred, caption='Segmented Image.', use_column_width=True)
+
+        
+        
