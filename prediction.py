042540b5d5585aabc3b62fd79e50016ef7a78580
Ankush Pandey <ap539813@gmail.com>
2022-07-02 09:32:12 +0530

    Added matplotlib imshow instead of st.image

--- prediction.py
+++ prediction.py
@@ -1,3 +1,4 @@
+from matplotlib import pyplot as plt
 from create_Segmentation_model import my_Unet
 from important_variables import img_height, img_width
 import streamlit as st
@@ -9,27 +10,40 @@ import numpy as np
 def segment_image(model_path):
     model = my_Unet(model_path)
 
-    input_col, output_col = st.columns([1, 1])
+    uploaded_file = st.file_uploader("Choose an image...", type="jpg")
 
+    input_col, output_col = st.columns([1, 1])
     input_col.markdown('## Input Image')
     output_col.markdown('## Segmented Image')
-    uploaded_file = input_col.file_uploader("Choose an image...", type="jpg")
+    
     if uploaded_file is not None:
         # image = cv2.imread(uploaded_file)
         image = Image.open(uploaded_file)
-        input_col.image(image, caption='Input Image.', use_column_width=True)
-        st.write("")
+
         image = image.resize((img_width, img_height))
+        # input_col.image(image, caption='Input Image.', use_column_width=True)
+        fig, axes = plt.subplots(dpi = 150)
+        axes.imshow(image)
+        plt.axis('off')
+        input_col.pyplot(fig)
+        st.write("")
+        
         
         image = np.expand_dims(image, 0)/255
 
         
         pred = np.argmax(model.predict(image), axis=3)[0,:,:]
+
+        fig, axes = plt.subplots()
+        axes.imshow(pred)
+        plt.axis('off')
+        output_col.pyplot(fig)
+
         
 
         print(pred.shape)
 
-        output_col.image(pred, caption='Segmented Image.', use_column_width=True)
+        # output_col.image(pred, caption='Segmented Image.', use_column_width=True)
 
         
         
